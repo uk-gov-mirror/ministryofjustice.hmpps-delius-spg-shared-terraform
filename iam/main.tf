0f0f791c19978fdb85b89727723cec392eeb2bc0
smjmoj <smjmoj@gmail.com>
2018-12-07 13:44:51 +0000

    fixed variable source

--- iam/main.tf
+++ iam/main.tf
@@ -57,7 +57,7 @@ module "iam" {
   source                   = "../modules/iam"
   spg_app_name             = "${local.spg_app_name}"
   environment_identifier   = "${local.environment_identifier}"
-  tags                     = "${merge(var.tags, map("sub-project", "${var.spg_app_name}"))}"
+  tags                     = "${merge(var.tags, map("sub-project", "${local.spg_app_name}"))}"
   ec2_role_policy_file     = "${file("../policies/ec2_role_policy.json")}"
   ecs_role_policy_file     = "${file("../policies/ecs_role_policy.json")}"
   ec2_policy_file          = "ec2_policy.json"
