e9db2cb7d1770aa9fdb4b1055c7aeaaf692d9786
Paul Crimes <paul.crimes@solirius.com>
2019-04-18 10:03:02 +0100

    fixed output var names

--- ecr/output.tf
+++ ecr/output.tf
@@ -3,14 +3,14 @@
 ####################################################
 
 output "ecr_repository_url" {
-  value = "${module.ecr.repository_url}"
+  value = "${module.ecr.repo_repository_url}"
 }
 
 output "ecr_repository_arn" {
-  value = "${module.ecr.repository_arn}"
+  value = "${module.ecr.repo_repository_arn}"
 }
 
 output "ecr_repository_name" {
-  value = "${module.ecr.repository_name}"
+  value = "${module.ecr.repo_repository_name}"
 }
 
