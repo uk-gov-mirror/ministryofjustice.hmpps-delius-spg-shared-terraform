33e17fa61c2484f079a78722a65ab3450b16e977
Paul Crimes <paul.crimes@solirius.com>
2019-06-27 15:31:32 +0100

    Tidied up firewall rules and allowed traffic between crc and iso/mpx

--- security-groups/external-instance.tf
+++ security-groups/external-instance.tf
@@ -3,119 +3,58 @@
 #-------------------------------------------------------------
 
 
-//webports not used for nginx / haproxy
-//#-------------------------------------------------------------
-//### port 80
-//#-------------------------------------------------------------
-//resource "aws_security_group_rule" "external_inst_ingress_http" {
-//  security_group_id        = "${local.external_inst_sg_id}"
-//  type                     = "ingress"
-//  from_port                = 80
-//  to_port                  = 80
-//  protocol                 = "tcp"
-//  source_security_group_id = "${local.external_lb_sg_id}"
-//  description              = "${local.common_name}-instance-external-ingress-http"
-//}
-//
-//
-//
-//resource "aws_security_group_rule" "external_inst_egress_http" {
-//  security_group_id        = "${local.external_inst_sg_id}"
-//  type                     = "egress"
-//  from_port                = 80
-//  to_port                  = 80
-//  protocol                 = "tcp"
-//  source_security_group_id = "${local.internal_lb_sg_id}"
-//  description              = "${local.common_name}-instance-external-egress-http"
-//}
-//
-//
-//
-//
-//
-//
-//#-------------------------------------------------------------
-//### port 443
-//#-------------------------------------------------------------
-//resource "aws_security_group_rule" "external_inst_ingress_https" {
-//  security_group_id        = "${local.external_inst_sg_id}"
-//  type                     = "ingress"
-//  from_port                = 443
-//  to_port                  = 443
-//  protocol                 = "tcp"
-//  source_security_group_id = "${local.external_lb_sg_id}"
-//  description              = "${local.common_name}-instance-external-ingress-https"
-//}
-//
-//resource "aws_security_group_rule" "external_inst_egress_https" {
-//  security_group_id        = "${local.external_inst_sg_id}"
-//  type                     = "egress"
-//  from_port                = 443
-//  to_port                  = 443
-//  protocol                 = "tcp"
-//  source_security_group_id = "${local.internal_lb_sg_id}"
-//  description              = "${local.common_name}-instance-external-egress-https"
-//}
-
-
-
-
-
-
-
-
-// egress is 8181 from haproxy to mpx-lb (all in one)
+#-------------------------------------------------------------
+### port 8181 (soap/rest/hawtio)
+#-------------------------------------------------------------
 resource "aws_security_group_rule" "external_inst_egress_httpsigned" {
-  security_group_id        = "${local.external_inst_sg_id}"
+  description              = "from-iso-to-mpxLB--forHaProxy-8181"
   type                     = "egress"
+  security_group_id        = "${local.external_inst_sg_id}"
+  source_security_group_id = "${local.internal_lb_sg_id}"
   from_port                = 8181
   to_port                  = 8181
   protocol                 = "tcp"
-  source_security_group_id = "${local.internal_lb_sg_id}"
-  description              = "${local.common_name}-instance-external-egress-httpsigned"
 }
 
-// egress is 8989 from iso to mpx-lb
+
+
+
+#-------------------------------------------------------------
+### port 8989 (soap from iso to mpx)
+#-------------------------------------------------------------
 resource "aws_security_group_rule" "external_inst_egress_httpunsigned" {
+  description              = "from-iso-to-mpxLB--forISOProxy-8989"
   security_group_id        = "${local.external_inst_sg_id}"
+  source_security_group_id = "${local.internal_lb_sg_id}"
   type                     = "egress"
   from_port                = 8989
   to_port                  = 8989
   protocol                 = "tcp"
-  source_security_group_id = "${local.internal_lb_sg_id}"
-  description              = "${local.common_name}-instance-external-egress-httpunsigned"
 }
 
 
 
+#-------------------------------------------------------------
+### port 9001 (soap/rest mutual TLS)
+#-------------------------------------------------------------
 
-//port 8989 not exposed on external
-//#-------------------------------------------------------------
-//### port 8989
-//#-------------------------------------------------------------
-//resource "aws_security_group_rule" "external_inst_ingress_http-8989" {
-//  security_group_id        = "${local.external_inst_sg_id}"
-//  type                     = "ingress"
-//  from_port                = 8989
-//  to_port                  = 8989
-//  protocol                 = "tcp"
-//  source_security_group_id = "${local.external_lb_sg_id}"
-//  description              = "${local.common_name}-instance-external-ingress-soapint"
-//}
-//
-//
-//resource "aws_security_group_rule" "external_inst_egress_http-8989" {
-//  security_group_id        = "${local.external_inst_sg_id}"
-//  type                     = "egress"
-//  from_port                = 8989
-//  to_port                  = 8989
-//  protocol                 = "tcp"
-//  source_security_group_id = "${local.internal_lb_sg_id}"
-//  description              = "${local.common_name}-instance-external-egress-soapint"
-//}
+resource "aws_security_group_rule" "external_inst_ingress_https" {
+  description              = "from-vpcCIDR-to-iso-for-crc-9001"
+  type                     = "ingress"
+  cidr_blocks              = ["${local.private_cidr_block}"]  #from crc
+  security_group_id        = "${local.external_inst_sg_id}"
+  from_port                = 9001
+  to_port                  = 9001
+  protocol                 = "tcp"
+}
 
+resource "aws_security_group_rule" "external_inst_egress_https" {
+  description              = "from-iso-to-vpcCIDR-for-crc-9001"
+  type                     = "egress"
+  security_group_id        = "${local.external_inst_sg_id}"
+  cidr_blocks              = ["${local.private_cidr_block}"] #from crc
+  from_port                = 9001
+  to_port                  = 9001
+  protocol                 = "tcp"
+}
 
-
-//workaround for terraform unable to get private IPs of NLB  as of 04/04/2019
-//there is a pull request waiting to fix this https://github.com/terraform-providers/terraform-provider-aws/pull/2901
-//hava put the rule in the main iso project
--- security-groups/internal-instance.tf
+++ security-groups/internal-instance.tf
@@ -1,74 +1,86 @@
-
-
-
 #-------------------------------------------------------------
 ### internal instance sg aka sg_spg_api_in - for use by mpx/all in one
 #-------------------------------------------------------------
+
+
+#-------------------------------------------------------------
+### port all (self)
+#-------------------------------------------------------------
 resource "aws_security_group_rule" "internal_inst_sg_ingress_self" {
+  description       = "self"
+  self              = true
   security_group_id = "${local.internal_inst_sg_id}"
   type              = "ingress"
   from_port         = 0
   to_port           = 0
   protocol          = -1
-  self              = true
-  description       = "self"
 }
 
 resource "aws_security_group_rule" "internal_inst_sg_egress_self" {
+  description       = "self"
   security_group_id = "${local.internal_inst_sg_id}"
+  self              = true
   type              = "egress"
   from_port         = 0
   to_port           = 0
   protocol          = -1
-  self              = true
-  description       = "self"
 }
 
-//for when allinone.iso calls allinone.mpx via loadbalancer
+
+
+#-------------------------------------------------------------
+### port 8989
+#-------------------------------------------------------------
 resource "aws_security_group_rule" "internal_inst_sg_ingress_unsigned_soap" {
-  security_group_id        = "${local.internal_inst_sg_id}"
+  description              = "from-mpxLB-to-mpx-8989"
   type                     = "ingress"
+  source_security_group_id = "${local.internal_lb_sg_id}"
+  security_group_id        = "${local.internal_inst_sg_id}"
   from_port                = "8989"
   to_port                  = "8989"
   protocol                 = "tcp"
-  source_security_group_id = "${local.internal_lb_sg_id}"
-  description              = "${local.common_name}-instance-internal-sg"
 }
 
-//for when iso/haproxy calls mpx via loadbalancer
+#-------------------------------------------------------------
+### port 8181 (soap/rest/hawtio)
+#-------------------------------------------------------------
 resource "aws_security_group_rule" "internal_inst_sg_ingress_signed_soap" {
+  description              = "from-mpxLB-mpx-8181"
+  source_security_group_id = "${local.internal_lb_sg_id}"
   security_group_id        = "${local.internal_inst_sg_id}"
   type                     = "ingress"
   from_port                = "8181"
   to_port                  = "8181"
   protocol                 = "tcp"
-  source_security_group_id = "${local.internal_lb_sg_id}"
-  description              = "${local.common_name}-instance-internal-sg"
 }
 
 
-//for when automation tests ssh in via loadbalancer...makes sense for single instance groups, multi instance will resolve randomly to an instance
+#-------------------------------------------------------------
+### port 2222 (ssh as used by MTS tests with virtuoso user)
+#-------------------------------------------------------------
 resource "aws_security_group_rule" "internal_inst_sg_ingress_ssh_2222" {
-  security_group_id        = "${local.internal_inst_sg_id}"
+  description              = "from-mpxLB-mpx-2222"
   type                     = "ingress"
+  source_security_group_id = "${local.internal_lb_sg_id}"
+  security_group_id        = "${local.internal_inst_sg_id}"
   from_port                = "2222"
   to_port                  = "2222"
   protocol                 = "tcp"
-  source_security_group_id = "${local.internal_lb_sg_id}"
-  description              = "${local.common_name}-instance-internal-sg"
 }
 
 
 
-## Allow JMS access to SPG GW from loadbalancer
+#-------------------------------------------------------------
+### port 61616-61617 (JMS  nDelius & Alfresco)
+#-------------------------------------------------------------
 resource "aws_security_group_rule" "internal_inst_ingress_jms_private" {
-  security_group_id        = "${local.internal_inst_sg_id}"
+  description              = "from-mpxLB-mpx-JMS"
   type                     = "ingress"
+  source_security_group_id = "${local.internal_lb_sg_id}"
+  security_group_id        = "${local.internal_inst_sg_id}"
   protocol                 = "tcp"
   from_port                = "${var.spg_partnergateway_domain_ports["jms_broker"]}"
   to_port                  = "${var.spg_partnergateway_domain_ports["jms_broker_ssl"]}"
-  source_security_group_id = "${local.internal_lb_sg_id}"
-  description              = "JMS in from lb"
 }
 
 
@@ -76,11 +88,11 @@ resource "aws_security_group_rule" "internal_inst_ingress_jms_private" {
 ## Allow JMS access from SPG to ANY server in private cidr block with the port range specified by delius domain (so includes spg ports as well)
 #as of 7/may/2019 there is a generic all ports out bound rule, but this is likely to be reduced
 resource "aws_security_group_rule" "internal_inst_egress_jms_private" {
-  security_group_id        = "${local.internal_inst_sg_id}"
+  description              = "from-mpx-to-vpcCIDR-forDeliusLB-and-mpxLBorAmazonMQ-JMS"
   type                     = "egress"
+  security_group_id        = "${local.internal_inst_sg_id}"
+  cidr_blocks              = ["${local.private_cidr_block}"]
   protocol                 = "tcp"
   from_port                = "${local.weblogic_domain_ports["spg_jms_broker"]}"
   to_port                  = "${local.weblogic_domain_ports["spg_jms_broker_ssl"]}"
-  cidr_blocks              = ["${local.private_cidr_block}"]
-  description              = "JMS out to GW to ND JMS"
 }
