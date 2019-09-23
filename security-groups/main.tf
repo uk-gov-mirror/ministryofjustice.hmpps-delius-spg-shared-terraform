f456d7c586ad80763533e260137758fe3107dc54
Mark Butler <mgb777@outlook.com>
2019-09-23 14:29:36 +0100

    Rename sec-group-rules to security

--- security-groups/main.tf
+++ security-groups/main.tf
@@ -53,7 +53,7 @@ data "terraform_remote_state" "nat" {
 
 ### Extract the new security group rules
 #-------------------------------------------------------------
-data "terraform_remote_state" "sec-group-rules" {
+data "terraform_remote_state" "security" {
   backend = "s3"
 
   config {
@@ -92,9 +92,7 @@ locals {
   internal_inst_sg_id = "${local.sg_map_ids["internal_inst_sg_id"]}"
   external_lb_sg_id   = "${local.sg_map_ids["external_lb_sg_id"]}"
   external_inst_sg_id = "${local.sg_map_ids["external_inst_sg_id"]}"
-  //amazonmq_inst_sg_id = "${local.sg_map_ids["amazonmq_inst_sg_id"]}"
   amazonmq_inst_sg_id = "${data.terraform_remote_state.common.amazonmq_inst_sg_id}"
-  #spg_outbound_id     = "${local.sg_map_ids["outbound_sg_id"]}"
-  spg_outbound_id     = "${data.terraform_remote_state.sec-group-rules.spg_common_outbound_sg_id}"
+  spg_outbound_id     = "${data.terraform_remote_state.security.spg_common_outbound_sg_id}"
 
 }
