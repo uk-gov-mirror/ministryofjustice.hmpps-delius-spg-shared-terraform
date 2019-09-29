1fe3dad8c03fcf5649b79cf8c06a3a1574292cc9
Paul Crimes <paul.crimes@solirius.com>
2019-09-29 18:35:40 +0100

    improved SG descriptions

--- security-groups-and-rules/external-crc-loadbalancer.tf
+++ security-groups-and-rules/external-crc-loadbalancer.tf
@@ -52,7 +52,7 @@ resource "aws_security_group_rule" "crc_lb_2222_egress" {
 resource "aws_security_group_rule" "crc_lb_9001_psn_proxy_ingress" {
   count                   = "${(length(lookup(var.PO_SPG_FIREWALL_INGRESS_RULES, "PSNPROXY_A","") )>0) ? 1 : 0}"
   security_group_id        = "${aws_security_group.internal_crc_loadbalancer.id}"
-  description              = "from iso"
+  description              = "from psn proxy"
   type                     = "ingress"
     cidr_blocks              = ["${lookup(var.PO_SPG_FIREWALL_INGRESS_RULES,"PSNPROXY_A")}","${lookup(var.PO_SPG_FIREWALL_INGRESS_RULES,"PSNPROXY_B")}"]
   from_port                = 9001
@@ -64,7 +64,7 @@ resource "aws_security_group_rule" "crc_lb_9001_psn_proxy_ingress" {
 
 resource "aws_security_group_rule" "crc_lb_to_instance_9001_egress" {
   security_group_id = "${aws_security_group.internal_crc_loadbalancer.id}"
-  description = "to crc"
+  description = "lb to crc"
   type = "egress"
   source_security_group_id = "${aws_security_group.internal_crc_instance.id}"
   from_port = 9001
@@ -80,7 +80,7 @@ resource "aws_security_group_rule" "crc_lb_to_instance_9001_egress" {
 #-------------------------------------------------------------
 resource "aws_security_group_rule" "crc_lb_9001_ingress" {
   security_group_id        = "${aws_security_group.internal_crc_loadbalancer.id}"
-  description              = "from iso"
+  description              = "from iso vpc"
   type                     = "ingress"
   cidr_blocks              = ["${local.private_cidr_block}"]  #from iso / mpx-hybrid servers - TODO should this be a security group instead?
   from_port                = 9001
@@ -91,7 +91,7 @@ resource "aws_security_group_rule" "crc_lb_9001_ingress" {
 
 resource "aws_security_group_rule" "crc_lb_9001_egress" {
   security_group_id        = "${aws_security_group.internal_crc_loadbalancer.id}"
-  description              = "to crc"
+  description              = "lb to crc"
   type                     = "egress"
   cidr_blocks              = ["${local.private_cidr_block}"]  # tocrc servers - TODO should this be a secruity group
   from_port                = 9001
