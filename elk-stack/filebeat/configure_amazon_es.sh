a4ddde58a35f601c58b7cb9126c46d296b407a4c
Mark Butler <mark.butler@solirius.com>
2020-01-07 15:36:01 +0000

    First attempt to run the add-index-pattern script

--- elk-stack/filebeat/configure_amazon_es.sh
+++ elk-stack/filebeat/configure_amazon_es.sh
@@ -27,7 +27,7 @@ fi
 echo "TG_ENVIRONMENT_TYPE argument ${TG_ENVIRONMENT_TYPE}"
 
 
-#sh ./add_index-pattern.sh ${TG_ENVIRONMENT_TYPE}
+./add_index-pattern.sh ${TG_ENVIRONMENT_TYPE}
 #./add_template.sh ${TG_ENVIRONMENT_TYPE}
 #./add_pipeline.sh ${TG_ENVIRONMENT_TYPE}
 #./add_search-tabular.sh ${TG_ENVIRONMENT_TYPE}
