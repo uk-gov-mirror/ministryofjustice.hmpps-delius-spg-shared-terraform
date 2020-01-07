26bed08d6e38532396fa862bcb5b2b43f9b3ed47
Mark Butler <mark.butler@solirius.com>
2020-01-07 16:27:01 +0000

    test

--- elk-stack/filebeat/configure_amazon_es.sh
+++ elk-stack/filebeat/configure_amazon_es.sh
@@ -28,7 +28,7 @@ echo "TG_ENVIRONMENT_TYPE argument ${TG_ENVIRONMENT_TYPE}"
 
 echo "pwd is `pwd`"
 
-sh "./add_index-pattern.sh"
+./add_index-pattern.sh ${TG_ENVIRONMENT_TYPE}
 #./add_template.sh ${TG_ENVIRONMENT_TYPE}
 #./add_pipeline.sh ${TG_ENVIRONMENT_TYPE}
 #./add_search-tabular.sh ${TG_ENVIRONMENT_TYPE}
