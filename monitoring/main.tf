21f3475afbca1219183843806cd5cecae0c1ad50
Mark Butler <mark.butler@solirius.com>
2020-02-14 08:58:48 +0000

    Remove duplicate lambda resources from main.tf (as they now exist in lambdas.tf)

--- monitoring/main.tf
+++ monitoring/main.tf
@@ -42,36 +42,3 @@ resource "aws_sns_topic_subscription" "alarm_subscription" {
   protocol           = "lambda"
   endpoint           = "${aws_lambda_function.notify-slack.arn}"
 }
-
-### Lambda
-
-locals {
-  lambda_name = "spgw_alarm_slack_notification"
-}
-
-data "archive_file" "notify-slack-lambda" {
-  type               = "zip"
-  source_file        = "${path.module}/lambda/${local.lambda_name}.js"
-  output_path        = "${path.module}/files/${local.short_environment_name}-${local.lambda_name}.zip"
-}
-
-data "aws_iam_role" "lambda_exec_role" {
-  name = "lambda_exec_role"
-}
-
-resource "aws_lambda_function" "notify-slack" {
-  filename           = "${data.archive_file.notify-slack-lambda.output_path}"
-  function_name      = "${local.lambda_name}"
-  role               = "${data.aws_iam_role.lambda_exec_role.arn}"
-  handler            = "${local.lambda_name}.handler"
-  source_code_hash   = "${base64sha256(file("${data.archive_file.notify-slack-lambda.output_path}"))}"
-  runtime            = "nodejs12.x"
-}
-
-resource "aws_lambda_permission" "with_sns" {
-  statement_id        = "AllowExecutionFromSNS"
-  action              = "lambda:InvokeFunction"
-  function_name       = "${aws_lambda_function.notify-slack.arn}"
-  principal           = "sns.amazonaws.com"
-  source_arn          = "${aws_sns_topic.alarm_notification.arn}"
-}
