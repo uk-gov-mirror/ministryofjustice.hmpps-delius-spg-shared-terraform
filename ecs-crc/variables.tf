90e123bd08799bae3bb6030909edbccd5943ac14
Paul <paul.crimes@solirius.com>
2020-03-04 12:00:13 +0000

    fix minimum healthy deploy to 100%

--- ecs-crc/variables.tf
+++ ecs-crc/variables.tf
@@ -78,7 +78,7 @@ variable "esc_container_stop_timeout" {
 }
 
 variable "deployment_minimum_healthy_percent_crc" {
-  default = "0"
+  default = "100"
 
   //default as 0 to ensure ECS task deployments are always affected (crc is nearly always only 1 server)
   //there is no important data / processes to preserve as only MTS in test use crc and deployments are always scheduled
