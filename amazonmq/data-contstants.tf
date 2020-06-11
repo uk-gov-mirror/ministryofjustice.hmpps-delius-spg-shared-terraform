e45795fe6911b27f940480c79cebb670812dd83b
Martin Spasov <martin.y.spasov@gmail.com>
2020-06-11 14:25:19 +0100

    ALS-881 Set SPG AMI to fixed version (#117)
    Co-authored-by: Martin Spasov <martin.spasov@solirius.com>

--- amazonmq/data-contstants.tf
+++ amazonmq/data-contstants.tf
@@ -11,26 +11,9 @@ data "aws_acm_certificate" "cert" {
 ### Getting the latest amazon ami
 #-------------------------------------------------------------
 data "aws_ami" "amazon_ami" {
-  most_recent = true
-
   filter {
     name   = "description"
-    values = ["Amazon Linux AMI *"]
-  }
-
-  filter {
-    name   = "architecture"
-    values = ["x86_64"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  filter {
-    name   = "root-device-type"
-    values = ["ebs"]
+    values = ["amzn2-ami-ecs-hvm-2.0.20200603-x86_64-ebs"]
   }
 
   owners = ["591542846629"] # AWS
--- ecs-crc/data-constants.tf
+++ ecs-crc/data-constants.tf
@@ -11,26 +11,10 @@ data "aws_acm_certificate" "cert" {
 ### Getting the latest amazon ami
 #-------------------------------------------------------------
 data "aws_ami" "amazon_ami" {
-  most_recent = true
 
   filter {
     name   = "name"
-    values = ["*amzn2-ami-ecs-hvm*"]
-  }
-  
-  filter {
-    name   = "architecture"
-    values = ["x86_64"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  filter {
-    name   = "root-device-type"
-    values = ["ebs"]
+    values = ["amzn2-ami-ecs-hvm-2.0.20200603-x86_64-ebs"]
   }
 
   owners = ["591542846629"] # AWS
