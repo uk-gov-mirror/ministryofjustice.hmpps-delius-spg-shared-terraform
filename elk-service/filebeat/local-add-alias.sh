2a3207b3c2ba3969cc4eb5a19a65a1117de9da16
Mark Butler <mark.butler@solirius.com>
2020-07-07 18:50:05 +0100

    Reindexing modifications

--- elk-service/filebeat/local-add-alias.sh
+++
@@ -1,7 +0,0 @@
-curl -k -XPOST "http://localhost:9200/_aliases" -H 'kbn-xsrf: true' -H 'Content-Type: application/json' -d'
-{
-    "actions" : [
-        { "add" : { "index" : "spg-audit-7.1.1*", "alias" : "spg-index-alias" } }
-    ]
-}
-'
--- elk-service/filebeat/local-removeall.sh
+++ elk-service/filebeat/local-removeall.sh
@@ -1,6 +1,19 @@
+#Index Patterns
 curl -k -XDELETE "http://localhost:5601/api/saved_objects/index-pattern/spg-audit" -H 'kbn-xsrf: true' -H 'Content-Type: application/json'
-curl -k -XDELETE "http://localhost:5601/api/saved_objects/search/spg-audit-tabular" -H 'kbn-xsrf: true' -H 'Content-Type: application/json'
+curl -k -XDELETE "http://localhost:5601/api/saved_objects/index-pattern/repair-spg-audit" -H 'kbn-xsrf: true' -H 'Content-Type: application/json'
+
+#Indexes
 curl -k -H 'Content-Type: application/json' -XDELETE 'http://localhost:9200/spg-audit*'
-curl -k -H 'Content-Type: application/json' -XDELETE 'http://localhost:9200/_ingest/pipeline/spg-audit'
+curl -k -H 'Content-Type: application/json' -XDELETE 'http://localhost:9200/repair-spg-audit*'
+
+#Templates
 curl -k -XDELETE "http://localhost:9200/_template/spg-audit" -H 'Content-Type: application/json'
+curl -k -XDELETE "http://localhost:9200/_template/repair-spg-audit" -H 'Content-Type: application/json'
+
+#Pipeline
+curl -k -H 'Content-Type: application/json' -XDELETE 'http://localhost:9200/_ingest/pipeline/spg-audit'
+
+#Search
+curl -k -XDELETE "http://localhost:5601/api/saved_objects/search/spg-audit-tabular" -H 'kbn-xsrf: true' -H 'Content-Type: application/json'
+
 
