11ca455a3cbcf14b5560eb6faaa89fdefd11ab3d
Martin Spasov <martin.spasov@solirius.com>
2020-10-26 10:48:15 +0000

    Update ecs-crc module

--- ecs-crc/ecs--service.tf
+++ ecs-crc/ecs--service.tf
@@ -1,23 +1,23 @@
-
 ############################################
 # CREATE ECS CLUSTER FOR spg
 ############################################
 # ##### ECS Cluster
 
 module "ecs_cluster" {
-  source       = "git::https://github.com/ministryofjustice/hmpps-terraform-modules.git?ref=master//modules//ecs//ecs_cluster"
-  cluster_name = "${local.common_name}"
+  source       = "git::https://github.com/ministryofjustice/hmpps-terraform-modules.git//modules/ecs/ecs_cluster?ref=terraform-0.12"
+  cluster_name = local.common_name
 }
+
 ############################################
 # CREATE LOG GROUPS FOR CONTAINER LOGS
 ############################################
 
 module "create_loggroup" {
-  source                   = "git::https://github.com/ministryofjustice/hmpps-terraform-modules.git?ref=master//modules//cloudwatch//loggroup"
-  log_group_path           = "${local.short_environment_name}"
+  source                   = "git::https://github.com/ministryofjustice/hmpps-terraform-modules.git//modules/cloudwatch/loggroup?ref=terraform-0.12"
+  log_group_path           = local.short_environment_name
   loggroupname             = "${local.app_name}-${local.app_submodule}"
-  cloudwatch_log_retention = "${local.cloudwatch_log_retention}"
-  tags                     = "${local.tags}"
+  cloudwatch_log_retention = local.cloudwatch_log_retention
+  tags                     = local.tags
 }
 
 ############################################
@@ -25,11 +25,11 @@ module "create_loggroup" {
 ############################################
 
 module "create_application_loggroup" {
-  source                   = "git::https://github.com/ministryofjustice/hmpps-terraform-modules.git?ref=master//modules//cloudwatch//loggroup"
-  log_group_path           = "${local.short_environment_name}"
+  source                   = "git::https://github.com/ministryofjustice/hmpps-terraform-modules.git//modules/cloudwatch/loggroup?ref=terraform-0.12"
+  log_group_path           = local.short_environment_name
   loggroupname             = "${local.app_name}-${local.app_submodule}-application"
-  cloudwatch_log_retention = "${local.cloudwatch_log_retention}"
-  tags                     = "${local.tags}"
+  cloudwatch_log_retention = local.cloudwatch_log_retention
+  tags                     = local.tags
 }
 
 ############################################
@@ -37,11 +37,11 @@ module "create_application_loggroup" {
 ############################################
 
 module "create_infrastructure_loggroup" {
-  source                   = "git::https://github.com/ministryofjustice/hmpps-terraform-modules.git?ref=master//modules//cloudwatch//loggroup"
-  log_group_path           = "${local.short_environment_name}"
+  source                   = "git::https://github.com/ministryofjustice/hmpps-terraform-modules.git//modules/cloudwatch/loggroup?ref=terraform-0.12"
+  log_group_path           = local.short_environment_name
   loggroupname             = "${local.app_name}-${local.app_submodule}-infrastructure"
-  cloudwatch_log_retention = "${local.cloudwatch_log_retention}"
-  tags                     = "${local.tags}"
+  cloudwatch_log_retention = local.cloudwatch_log_retention
+  tags                     = local.tags
 }
 
 ############################################
@@ -51,45 +51,40 @@ module "create_infrastructure_loggroup" {
 #with predefined elb
 
 module "app_service" {
-  source                             = "git::https://github.com/ministryofjustice/hmpps-terraform-modules.git?ref=master//modules//ecs/ecs_service//withloadbalancer//elb"
-  servicename                        = "${local.common_name}"
-  clustername                        = "${module.ecs_cluster.ecs_cluster_id}"
-  ecs_service_role                   = "${local.ecs_service_role}"
-  elb_name                           = "${module.create_app_elb.environment_elb_name}"
+  source                             = "git::https://github.com/ministryofjustice/hmpps-terraform-modules.git//modules/ecs/ecs_service/withloadbalancer/elb?ref=terraform-0.12"
+  servicename                        = local.common_name
+  clustername                        = module.ecs_cluster.ecs_cluster_id
+  ecs_service_role                   = local.ecs_service_role
+  elb_name                           = module.create_app_elb.environment_elb_name
   containername                      = "${local.app_name}-${local.app_submodule}"
-  containerport                      = "${local.backend_app_port}"
-  task_definition_family             = "${module.app_task_definition.task_definition_family}"
-  task_definition_revision           = "${module.app_task_definition.task_definition_revision}"
-  current_task_definition_version    = "${data.aws_ecs_task_definition.app_task_definition.revision}"
-  service_desired_count              = "${local.service_desired_count}"
-  deployment_minimum_healthy_percent = "${var.deployment_minimum_healthy_percent_crc}"
+  containerport                      = local.backend_app_port
+  task_definition_family             = module.app_task_definition.task_definition_family
+  task_definition_revision           = module.app_task_definition.task_definition_revision
+  current_task_definition_version    = data.aws_ecs_task_definition.app_task_definition.revision
+  service_desired_count              = local.service_desired_count
+  deployment_minimum_healthy_percent = var.deployment_minimum_healthy_percent_crc
 }
-
-
-
 
 ############################################
 # CREATE USER DATA FOR EC2 RUNNING SERVICES
 ############################################
 
 data "template_file" "user_data" {
-  template = "${file("${local.user_data}")}"
+  template = file(local.user_data)
 
-  vars {
-    ebs_device           = "${local.ebs_device_name}"
-    app_name             = "${local.app_name}-${local.app_submodule}}"
-    env_identifier       = "${local.environment_identifier}"
-    short_env_identifier = "${local.short_environment_name}"
-    cluster_name         = "${module.ecs_cluster.ecs_cluster_name}"
-    log_group_application_name = "${module.create_application_loggroup.loggroup_name}"
-    log_group_infrastructure_name = "${module.create_infrastructure_loggroup.loggroup_name}"
-    container_name       = "${local.app_name}-${local.app_submodule}"
-    bastion_inventory    = "${var.bastion_inventory}"
-
-
-    data_volume_host_path      = "${local.data_volume_host_path}"
-    data_volume_name           = "${local.data_volume_name}"
-    esc_container_stop_timeout = "${var.esc_container_stop_timeout}"
+  vars = {
+    ebs_device                    = local.ebs_device_name
+    app_name                      = "${local.app_name}-${local.app_submodule}}"
+    env_identifier                = local.environment_identifier
+    short_env_identifier          = local.short_environment_name
+    cluster_name                  = module.ecs_cluster.ecs_cluster_name
+    log_group_application_name    = module.create_application_loggroup.loggroup_name
+    log_group_infrastructure_name = module.create_infrastructure_loggroup.loggroup_name
+    container_name                = "${local.app_name}-${local.app_submodule}"
+    bastion_inventory             = var.bastion_inventory
+    data_volume_host_path         = local.data_volume_host_path
+    data_volume_name              = local.data_volume_name
+    esc_container_stop_timeout    = var.esc_container_stop_timeout
   }
 }
 
@@ -98,34 +93,34 @@ data "template_file" "user_data" {
 ############################################
 
 module "launch_cfg" {
-  source                      = "git::https://github.com/ministryofjustice/hmpps-terraform-modules.git?ref=master//modules//launch_configuration//blockdevice"
-  launch_configuration_name   = "${local.common_name}"
-  image_id                    = "${local.ami_id}"
-  instance_type               = "${local.instance_type}"
-  volume_size                 = "${local.volume_size}"
-  instance_profile            = "${local.instance_profile}"
-  key_name                    = "${local.ssh_deployer_key}"
-  ebs_device_name             = "${local.ebs_device_name}"
-  ebs_volume_type             = "${local.ebs_volume_type}"
-  ebs_volume_size             = "${local.ebs_volume_size}"
-  ebs_encrypted               = "${local.ebs_encrypted}"
-  associate_public_ip_address = "${local.associate_public_ip_address}"
-  security_groups             = ["${local.instance_security_groups}"]
-  user_data                   = "${data.template_file.user_data.rendered}"
-
+  source                      = "git::https://github.com/ministryofjustice/hmpps-terraform-modules.git//modules/launch_configuration/blockdevice?ref=terraform-0.12"
+  launch_configuration_name   = local.common_name
+  image_id                    = local.ami_id
+  instance_type               = local.instance_type
+  volume_size                 = local.volume_size
+  instance_profile            = local.instance_profile
+  key_name                    = local.ssh_deployer_key
+  ebs_device_name             = local.ebs_device_name
+  ebs_volume_type             = local.ebs_volume_type
+  ebs_volume_size             = local.ebs_volume_size
+  ebs_encrypted               = local.ebs_encrypted
+  associate_public_ip_address = local.associate_public_ip_address
+  security_groups             = local.instance_security_groups
+  user_data                   = data.template_file.user_data.rendered
 }
 
 ############################################
 # CREATE AUTO SCALING GROUP
 ############################################
-
+// TODO - change source to ref=terraform-0.12
 module "auto_scale" {
-  source               = "git::https://github.com/ministryofjustice/hmpps-terraform-modules.git?ref=master//modules//autoscaling//group//default"
-  asg_name             = "${local.common_name}"
-  subnet_ids           = ["${local.private_subnet_ids}"]
-  asg_min              = "${local.asg_min}"
-  asg_max              = "${local.asg_max}"
-  asg_desired          = "${local.asg_desired}"
-  launch_configuration = "${module.launch_cfg.launch_name}"
-  tags                 = "${local.tags}"
+  source               = "git::https://github.com/ministryofjustice/hmpps-terraform-modules.git//modules/autoscaling/group/default?ref=terraform-12-tags"
+  asg_name             = local.common_name
+  subnet_ids           = local.private_subnet_ids
+  asg_min              = local.asg_min
+  asg_max              = local.asg_max
+  asg_desired          = local.asg_desired
+  launch_configuration = module.launch_cfg.launch_name
+  tags                 = local.tags
 }
+
--- modules/loadbalancer/elb/create_elb/main.tf
+++ modules/loadbalancer/elb/create_elb/main.tf
@@ -1,24 +1,41 @@
 resource "aws_elb" "environment" {
-  name            = "${var.name}"
-  subnets         = ["${var.subnets}"]
-  internal        = "${var.internal}"
-  security_groups = ["${var.security_groups}"]
+  name            = var.name
+  subnets         = var.subnets
+  internal        = var.internal
+  security_groups = var.security_groups
 
   cross_zone_load_balancing   = "${var.cross_zone_load_balancing}"
   idle_timeout                = "${var.idle_timeout}"
   connection_draining         = "${var.connection_draining}"
   connection_draining_timeout = "${var.connection_draining_timeout}"
 
-  listener = ["${var.listener}"]
-
-  access_logs = {
-    bucket        = "${var.bucket}"
-    bucket_prefix = "${var.bucket_prefix}"
-    interval      = "${var.interval}"
+  dynamic "listener" {
+    for_each = var.listener
+    content {
+      instance_port                = listener.value.instance_port
+      instance_protocol            = listener.value.instance_protocol
+      lb_port                      = listener.value.lb_port
+      lb_protocol                  = listener.value.lb_protocol
+    }
   }
 
-  health_check = ["${var.health_check}"]
-  tags         = "${merge(var.tags, map("Name", format("%s", var.name)))}"
+  access_logs  {
+    bucket        = var.bucket
+    bucket_prefix = var.bucket_prefix
+    interval      = var.interval
+  }
 
+  dynamic "health_check" {
+    for_each = var.health_check
+    content {
+      target                  = health_check.value.target
+      interval                = health_check.value.interval
+      healthy_threshold       = health_check.value.healthy_threshold
+      unhealthy_threshold     = health_check.value.unhealthy_threshold
+      timeout                 = health_check.value.timeout
+    }
+  }
+
+  tags         = "${merge(var.tags, map("Name", format("%s", var.name)))}"
 
 }
